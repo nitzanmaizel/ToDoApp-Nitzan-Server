ac1f5015e8e564b04f411d31df1fe385fb610c07
unknown <nitzan@jama.com>
2021-05-14 02:19:13 +0300

    try small cahnge for heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-   res.json({ hi: "Hello World!!!" });
+   res.json({ by: "By World!!!" });
 });
 
 const PORT = process.env.PORT || 5000;
